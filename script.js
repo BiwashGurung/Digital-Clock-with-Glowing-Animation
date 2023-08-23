33d5f27a4a821f8727861101c2f08224ef07deef
BiwashGurung <varungrg10@gmail.com>
2023-08-23 18:07:41 +0545

    declared variable minutes

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ setInterval(()=>{
     const time = document.querySelector(".display #time");
     let date = new Date();
     let hours = date.getHours();
+    let minutes = date.getMinutes();
   
   });
